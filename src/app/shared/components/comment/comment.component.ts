6257e69f6f79c9ff70cf4a311d2ff0f86e687211
Orlan29 <bricebouadou@gmail.com>
2023-05-17 17:17:12 +0000

    Basic animation

--- src/app/shared/components/comment/comment.component.ts
+++ src/app/shared/components/comment/comment.component.ts
@@ -1,22 +1,46 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { Comment } from "../../../core/models/comment.model";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201, 157, 242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class CommentComponent implements OnInit{
   @Input() comments!: Comment[];
   commentCtrl!: FormControl;
   @Output() newComment = new EventEmitter<string>();
+  animationState: {[key: number]: 'default' | 'active'} = {};
 
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.commentCtrl = this.fb.control('', [Validators.required, Validators.minLength(10)])
+    this.commentCtrl = this.fb.control('', [Validators.required, Validators.minLength(10)]);
+    for(let index in this.comments) {
+      this.animationState[index] = 'default';
+    }
   }
 
   onLeaveComment() {
@@ -25,5 +49,13 @@ export class CommentComponent implements OnInit{
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+  }
+
+  onMouseEnter(index: number): void {
+    this.animationState[index] = 'active';
+  }
+
+  onMouseLeave(index: number): void {
+    this.animationState[index] = 'default';
   }
 }
