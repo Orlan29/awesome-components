2bc8a33b5a1642a4a23ee7c9cf9ea49886a561c4
firstuser <bricebouadou@gmail.com>
2023-05-20 16:23:46 +0000

    Void animated

--- src/app/shared/components/comment/comment.component.ts
+++ src/app/shared/components/comment/comment.component.ts
@@ -24,6 +24,18 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       ]),
       transition('active => default', [
         animate('500ms ease-in-out')
+      ]),
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+          'background-color': 'rgb(201, 157, 242)'
+        }),
+        animate('250ms ease-in-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+          'background-color': 'white'
+        }))
       ])
     ])
   ]
@@ -47,6 +59,13 @@ export class CommentComponent implements OnInit{
     if (this.commentCtrl.invalid) {
       return;
     }
+    const maxId = Math.max(...this.comments.map(comment => comment.id));
+    this.comments.unshift({
+      id: maxId,
+      comment: this.commentCtrl.value,
+      createdDate: new Date().toISOString(),
+      userId: 1
+    });
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
   }
